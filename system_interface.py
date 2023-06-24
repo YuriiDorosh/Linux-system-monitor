f2e121ca1c7ef98a21c42ccaf95591f0933a71e0
yurii <yuriydorosh2005@gmail.com>
2023-06-25 01:51:35 +0300

    Add ExtendedSystemInterface class for minimalize window

--- system_interface.py
+++ system_interface.py
@@ -11,7 +11,7 @@ class SystemInterface:
         disk_usage = self.system_info.get_disk_usage()
         gpu_usage = self.system_info.get_gpu_usage()
 
-        progress_bars = [f"CPU{i+1}: {load}%" for i, load in enumerate(cpu_load)] + [
+        progress_bars = [f"CPU{i + 1}: {load}%" for i, load in enumerate(cpu_load)] + [
             f"Memory: {memory_usage}%",
             f"Disk: {disk_usage}%",
             f"GPU: {gpu_usage}%",
@@ -22,3 +22,17 @@ class SystemInterface:
     def get_disk_info(self):
         disk_info = f"Disk Free Space: {self.system_info.get_disk_free_space():.2f} GB"
         return disk_info
+
+
+class ExtendedSystemInterface(SystemInterface):
+    def __init__(self):
+        super().__init__()
+
+    def get_average_cpu_load(self):
+        cpu_load = self.system_info.get_cpu_load()
+        average_load = round(sum(cpu_load) / len(cpu_load), 2)
+        return f"Average CPU Load: {average_load}%"
+
+    def get_gpu_usage_percentage(self):
+        gpu_usage = self.system_info.get_gpu_usage()
+        return f"GPU Usage: {gpu_usage}%"
