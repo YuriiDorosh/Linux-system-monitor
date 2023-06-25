b9ae60cbac914657885b0477a1cf0f7cfde1e8c4
yurii <yuriydorosh2005@gmail.com>
2023-06-25 12:51:44 +0300

    Change imports

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import sys
-from gui_interface import GuiInterface
-from console_interface import ConsoleInterface
+
+import gui
+import console
 
 
 def main():
     if len(sys.argv) > 1 and sys.argv[1] == "gui":
-        gui_interface = GuiInterface()
+        gui_interface = gui.gui_interface.GuiInterface()
         gui_interface.run()
     else:
-        console_interface = ConsoleInterface()
+        console_interface = console.console_interface.ConsoleInterface()
         console_interface.run()
 
 
