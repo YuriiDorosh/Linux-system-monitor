c47d976aa38ab41099d74e31ba0182fdd8cb5975
yurii <yuriydorosh2005@gmail.com>
2023-07-14 20:09:17 +0300

    UPDATE: now file use data from json file

--- src/settings.py
+++ src/settings.py
@@ -1,12 +1,17 @@
+import json
+
+
 class SystemInfo:
     """
+    A class representing system information and settings.
+
     Attributes:
-    cpu_percent_interval (float): The interval at which CPU load is measured, in seconds. By default, it is set to 0.1 seconds.
-        Adjust this value to control the frequency of CPU load updates. Higher values result in slower updates, while lower values result in faster updates.
+        cpu_percent_interval (float): The interval at which CPU load is measured, in seconds. By default, it is set to 0.1 seconds.
+            Adjust this value to control the frequency of CPU load updates. Higher values result in slower updates, while lower values result in faster updates.
 
     Notes:
         - The `cpu_percent_interval` attribute determines the time interval at which the CPU load is measured.
-        Adjust this value based on your desired frequency of CPU load updates.
+          Adjust this value based on your desired frequency of CPU load updates.
     """
 
     cpu_percent_interval: float = 0.1  # Default 0.1 (if more - slower, less - faster)
@@ -23,8 +28,10 @@ class ScreenRecording:
             You can modify this value to a smaller or larger frame rate depending on your requirements and system capabilities.
         monitor_resolution (str): The resolution of the monitor to be recorded. By default, it is set to "1920x1080".
             You can modify this value to match the desired monitor resolution for your screen recording.
+
     Usage:
-    To use the `ScreenRecording` class, create an instance of the class and access its attributes and methods.
+        To use the `ScreenRecording` class, create an instance of the class and access its attributes and methods.
+
     Notes:
         - You can modify the `video_extension` attribute to set a different default video extension for your recordings.
         - The supported video formats depend on the availability and configuration of FFmpeg on your system.
@@ -45,7 +52,7 @@ class ScreenRecording:
     # .3gp - 3GPP multimedia container format.
     # .ts - MPEG Transport Stream container format.
 
-    frame_rate: int = 30  # Default 30, but you can change this value to a smaller side or, if your system allows, to a larger one
+    frame_rate: int = 30  # Default 30, but you can change this value to a smaller or, if your system allows, to a larger one
 
     monitor_resolution: str = "1920x1080"  # Default 1920x1080, but you can try:
     # Standard Definition(SD):
@@ -71,3 +78,22 @@ class ScreenRecording:
     # 8K:
     #
     # 7680x4320
+
+
+# Load data from the JSON file
+with open("settings.json") as json_file:
+    data = json.load(json_file)
+
+# Update attributes in the SystemInfo class
+SystemInfo.cpu_percent_interval = data.get(
+    "cpu_percent_interval", SystemInfo.cpu_percent_interval
+)
+
+# Update attributes in the ScreenRecording class
+ScreenRecording.video_extension = data.get(
+    "video_format", ScreenRecording.video_extension
+)
+ScreenRecording.frame_rate = data.get("fps", ScreenRecording.frame_rate)
+ScreenRecording.monitor_resolution = data.get(
+    "monitor_resolution", ScreenRecording.monitor_resolution
+)
