c17da667b064b61d08a444c511561473b074c692
yurii <yuriydorosh2005@gmail.com>
2023-07-14 18:05:03 +0300

    FIX: if elif else construction

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
 import sys
 
-
-from interfaces import gui, console
+from interfaces import gui, console, greeting
 
 
 def main():
-    if sys.argv[1] == "--gui" or sys.argv[1] == "-g":
-        gui_interface = gui.gui_interface.GuiInterface()
-        gui_interface.run()
-    elif sys.argv[1] == "--console" or sys.argv[1] == "-c":
-        console_interface = console.console_interface.ConsoleInterface()
-        console_interface.run()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--gui" or sys.argv[1] == "-g":
+            gui_interface = gui.gui_interface.GuiInterface()
+            gui_interface.run()
+        elif sys.argv[1] == "--console" or sys.argv[1] == "-c":
+            console_interface = console.console_interface.ConsoleInterface()
+            console_interface.run()
+        else:
+            greeting_window = greeting.window.MainWindow()
+            greeting_window.run()
     else:
-        pass
+        greeting_window = greeting.window.MainWindow()
+        greeting_window.run()
 
 
 if __name__ == "__main__":
