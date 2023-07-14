f88b5d79503931da34f1440e3539fb72483f9589
yurii <yuriydorosh2005@gmail.com>
2023-07-14 15:41:14 +0300

    ADD:memory usage on GB, max memory and frequency of CPU and GPU

--- src/system/system_info.py
+++ src/system/system_info.py
@@ -14,6 +14,8 @@ class SystemInfo:
     Methods:
         get_cpu_load(): Returns the CPU load as a percentage for each CPU core.
         get_memory_usage(): Returns the memory usage as a percentage.
+        get_memory_usage_gb(): Returns the memory usage in gigabytes (GB).
+        get_max_memory(): Returns the maximum available memory in gigabytes (GB).
         get_disk_usage(): Returns the disk usage as a percentage.
         get_gpu_usage(): Returns the GPU usage as a percentage if an NVIDIA GPU is available, otherwise returns 0.
         get_disk_free_space(): Returns the available free space on the disk in gigabytes (GB).
@@ -25,6 +27,8 @@ class SystemInfo:
         2. Access the system information methods:
             cpu_load = system_info.get_cpu_load()
             memory_usage = system_info.get_memory_usage()
+            memory_usage_gb = system_info.get_memory_usage_gb()
+            max_memory_gb = system_info.get_max_memory()
             disk_usage = system_info.get_disk_usage()
             gpu_usage = system_info.get_gpu_usage()
             disk_free_space = system_info.get_disk_free_space()
@@ -34,6 +38,7 @@ class SystemInfo:
         - The pynvml module is used for GPU information and requires an NVIDIA GPU and the appropriate drivers to be installed.
         - The CPU load is provided as a percentage for each CPU core.
         - Memory usage and disk usage are reported as percentages.
+        - Memory usage is also reported in gigabytes (GB).
         - Disk free space is reported in gigabytes (GB).
     """
 
@@ -69,6 +74,38 @@ class SystemInfo:
             print(memory_usage)  # Output: 65.3
         """
         return psutil.virtual_memory().percent
+
+    @staticmethod
+    def get_memory_usage_gb():
+        """
+        Returns the memory usage in gigabytes (GB).
+
+        Returns:
+            float: Memory usage in gigabytes (GB).
+
+        Example:
+            memory_usage_gb = SystemInfo.get_memory_usage_gb()
+            print(memory_usage_gb)  # Output: 4.2
+        """
+        memory = psutil.virtual_memory()
+        memory_gb = memory.used / (1024 ** 3)
+        return memory_gb
+
+    @staticmethod
+    def get_max_memory():
+        """
+        Returns the maximum available memory in gigabytes (GB).
+
+        Returns:
+            float: Maximum available memory in gigabytes (GB).
+
+        Example:
+            max_memory_gb = SystemInfo.get_max_memory()
+            print(max_memory_gb)  # Output: 16.0
+        """
+        memory = psutil.virtual_memory()
+        max_memory_gb = memory.total / (1024 ** 3)
+        return max_memory_gb
 
     @staticmethod
     def get_disk_usage():
@@ -113,6 +150,49 @@ class SystemInfo:
         return 0
 
     @staticmethod
+    def get_cpu_frequency():
+        """
+        Returns the current frequency of the CPU in MHz.
+        ...
+        """
+        return psutil.cpu_freq().current
+
+    def get_gpu_frequency(self):
+        """
+        Returns the current frequency of the GPU if an NVIDIA GPU is available, otherwise returns 0.
+        ...
+        """
+        if not self.pynvml_initialized:
+            pynvml.nvmlInit()
+            self.pynvml_initialized = True
+
+        device_count = pynvml.nvmlDeviceGetCount()
+
+        for i in range(device_count):
+            handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+            device_name = pynvml.nvmlDeviceGetName(handle)
+
+            if "NVIDIA" in device_name:
+                clock_info = pynvml.nvmlDeviceGetClockInfo(handle, pynvml.NVML_CLOCK_GRAPHICS)
+                return clock_info / 1000  # Convert to MHz
+
+        return 0
+
+    @staticmethod
+    def get_max_cpu_frequency():
+        """
+        Returns the maximum CPU frequency in megahertz (MHz).
+
+        Returns:
+            int: Maximum CPU frequency in megahertz (MHz).
+
+        Example:
+            max_cpu_frequency = SystemInfo.get_max_cpu_frequency()
+            print(max_cpu_frequency)  # Output: 4000
+        """
+        return psutil.cpu_freq().max
+
+    @staticmethod
     def get_disk_free_space():
         """
         Returns the available free space on the disk in gigabytes (GB).
@@ -125,4 +205,4 @@ class SystemInfo:
             print(disk_free_space)  # Output: 214.5
         """
         disk_usage = psutil.disk_usage("/")
-        return disk_usage.free / (1024**3)
+        return disk_usage.free / (1024 ** 3)
