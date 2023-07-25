5b9b4b61c86a66aeb161d154df62636f764648ba
yurii <yuriydorosh2005@gmail.com>
2023-07-25 16:23:17 +0300

    FIX: launching on Windows OS

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import sys
 import platform
-import pynvml
-from interfaces import gui, console, greeting
 
 
 def check_nvidia_gpu():
+    import pynvml
+
     """
     Checks the presence of an NVIDIA graphics card on the system.
+
+    This function uses the pynvml library to check for the presence of NVIDIA GPUs on the system.
 
     Returns:
         bool: True if an NVIDIA graphics card is present, otherwise False.
@@ -27,11 +29,20 @@ def main():
 
     It checks the operating system and the presence of an NVIDIA graphics card,
     then launches the appropriate interface or displays relevant messages.
+
+    This program supports running in Linux environments with NVIDIA graphics cards.
+
+    Usage:
+        To run the GUI version: python main.py --gui
+        To run the console version: python main.py --console
+
     """
     if platform.system() == "Windows":
         print("This program is intended for Linux and is not supported on Windows.")
         sys.exit(1)
     else:
+        from interfaces import gui, console, greeting
+
         if not check_nvidia_gpu():
             print(
                 "This program works only with NVIDIA video cards."
