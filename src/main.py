29ffe55b84d6343efdbffd77ddc3138207637477
yurii <yuriydorosh2005@gmail.com>
2023-07-25 16:02:23 +0300

    FIX: else statement(launching greeting window with settings)

--- src/main.py
+++ src/main.py
@@ -46,9 +46,9 @@ def main():
                 elif sys.argv[1] == "--console" or sys.argv[1] == "-c":
                     console_interface = console.console_interface.ConsoleInterface()
                     console_interface.run()
-                else:
-                    greeting_window = greeting.window.MainWindow()
-                    greeting_window.run()
+            else:
+                greeting_window = greeting.window.MainWindow()
+                greeting_window.run()
 
 
 if __name__ == "__main__":
