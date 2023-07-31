7b45e35df35075d4ddf607472b3101358f1bbf2c
yurii <yuriydorosh2005@gmail.com>
2023-07-31 16:38:25 +0300

    UPDATE: a menu to choose to run the project after building

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Step 1: Installing dependencies
+# Linux System Monitor Build and Run Script
+
+# Step 1: Install dependencies
 sudo apt update
 sudo apt install -y ffmpeg git python3 python3-pip
 
-# Checking if the repository is already cloned
+# Step 2: Check if the repository is already cloned
 if [ -d "Linux-system-monitor" ]; then
   echo "The repository has already been cloned, let's proceed to the next steps."
 else
@@ -14,13 +16,41 @@ else
   cd Linux-system-monitor/
 fi
 
-# Step 2: Create and activate the virtual environment
+# Step 3: Create and activate the virtual environment
 python3 -m venv env
 source env/bin/activate
 
-# Step 3: Installing the required Python packages
+# Step 4: Install the required Python packages
 pip3 install -r requirements.txt
 
-# Step 4: Launch the project
+# Step 5: Launch the project
 cd src/
-python3 main.py
+
+# Display menu with options
+echo "Select the version to run:"
+echo "1) GUI version"
+echo "2) Console version"
+echo "3) Default version (Greeting window)"
+echo "4) Help - Display project help"
+
+# Read user input for the desired version
+read -p "Enter your choice [1-4]: " choice
+
+# Execute the selected option
+case $choice in
+  1)
+    python3 main.py --gui
+    ;;
+  2)
+    python3 main.py --console
+    ;;
+  3)
+    python3 main.py
+    ;;
+  4)
+    python3 main.py --help
+    ;;
+  *)
+    echo "Invalid choice. Exiting."
+    ;;
+esac
