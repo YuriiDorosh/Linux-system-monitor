1303db8144ad9b999134171f9988a9839037f041
yurii <yuriydorosh2005@gmail.com>
2023-07-31 15:05:40 +0300

    ADD: text file with supporting information on the project

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
 import platform
+import os
 
 
 def check_nvidia_gpu():
@@ -23,6 +24,24 @@ def check_nvidia_gpu():
         return False
 
 
+def get_help_text():
+    """
+    Retrieves the content of the help file.
+
+    This function reads the 'help.txt' file located in the project's top-level directory (beside 'src/').
+    If the file exists, its content is returned; otherwise, a message indicating that the file is not found is returned.
+
+    Returns:
+        str: The content of the 'help.txt' file, or a message stating that the file is not found.
+    """
+    help_file_path = os.path.join(os.path.dirname(__file__), "..", "help.txt")
+    if os.path.isfile(help_file_path):
+        with open(help_file_path, "r") as file:
+            return file.read()
+    else:
+        return "Help file not found."
+
+
 def main():
     """
     Main function to run the program.
@@ -33,20 +52,28 @@ def main():
     This program supports running in Linux environments with NVIDIA graphics cards.
 
     Usage:
-        To run the GUI version: python main.py --gui
-        To run the console version: python main.py --console
-
+        To run the GUI version: python(3) __main__.py --gui or python(3) __main__.py -g
+        To run the console version: python(3) __main__.py --console or python(3) __main__.py -c
+        To display this help message: python(3) __main__.py --help or python(3) __main__.py -h
     """
+    if "--help" in sys.argv or "-h" in sys.argv:
+        print(main.__doc__)
+        help_text = get_help_text()
+        print(f"\n{'*' * 50}\n" "\nAdditional Help Text:" "\n")
+        print(help_text)
+        sys.exit(0)
+
     if platform.system() == "Windows":
         print("This program is intended for Linux and is not supported on Windows.")
         sys.exit(1)
     else:
-        from interfaces import gui, console, greeting
+        from .interfaces import gui, console, greeting
 
         if not check_nvidia_gpu():
             print(
                 "This program works only with NVIDIA video cards."
                 "\nIf you have an NVIDIA graphics card, check if the drivers are installed."
+                "\nTry(Ubuntu): sudo ubuntu-drivers autoinstall"
             )
             sys.exit(1)
         else:
