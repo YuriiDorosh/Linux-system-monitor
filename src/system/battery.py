3f02bd4e37fd4fd3ba00006a0e58beffd8448bd3
yurii <yuriydorosh2005@gmail.com>
2023-08-04 19:22:27 +0300

    REFACTOR: documentations, typing, black

--- src/system/battery.py
+++ src/system/battery.py
@@ -1,14 +1,34 @@
 import psutil
+from typing import Optional
 
 
 class Battery:
+    """
+    A class that provides methods to retrieve battery-related information.
+
+    Methods:
+        get_battery_status() -> Optional[str]: Retrieves battery status information.
+        get_battery_percent() -> Optional[float]: Retrieves battery percentage.
+
+    Usage:
+        1. Initialize an instance of the Battery class:
+            battery = Battery()
+
+        2. Access the methods to retrieve battery information:
+            battery_status = battery.get_battery_status()
+            battery_percent = battery.get_battery_percent()
+
+    Notes:
+        - The Battery class depends on the psutil module for battery information.
+    """
+
     @staticmethod
-    def get_battery_status():
+    def get_battery_status() -> Optional[str]:
         """
         Retrieves battery status information.
 
         Returns:
-            str or None: Battery status information or None if not supported.
+            str or None: Battery status information (e.g., "Charging", "Discharging") or None if not supported.
 
         Example:
             battery = Battery()
@@ -24,7 +44,7 @@ class Battery:
         return None
 
     @staticmethod
-    def get_battery_percent():
+    def get_battery_percent() -> Optional[float]:
         """
         Retrieves battery percentage.
 
