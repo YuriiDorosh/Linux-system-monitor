f6ab367a020a3847b283407f0e26100e83eeacfc
yurii <yuriydorosh2005@gmail.com>
2023-08-04 19:22:57 +0300

    REFACTOR: documentations, typing, black

--- src/system/checkers/nvidia_checker.py
+++ src/system/checkers/nvidia_checker.py
@@ -2,9 +2,30 @@ import pynvml
 
 
 class CheckNvidia:
+    """
+    A class that checks the presence of an NVIDIA graphics card on the system.
+
+    Methods:
+        is_nvidia_gpu_present() -> bool: Checks the presence of an NVIDIA graphics card on the system.
+
+    Usage:
+        1. Initialize an instance of the CheckNvidia class:
+            nvidia_checker = CheckNvidia()
+
+        2. Access the method to check for an NVIDIA GPU:
+            is_present = nvidia_checker.is_nvidia_gpu_present()
+            if is_present:
+                print("NVIDIA GPU is present.")
+            else:
+                print("No NVIDIA GPU found.")
+
+    Notes:
+        - The CheckNvidia class depends on the pynvml module for NVIDIA GPU information.
+    """
+
     def __init__(self):
         """
-        Initializes the CheckNvidia class.
+        Initializes the CheckNvidia class and initializes the pynvml library.
         """
         pynvml.nvmlInit()
 
@@ -15,7 +36,7 @@ class CheckNvidia:
         pynvml.nvmlShutdown()
 
     @staticmethod
-    def is_nvidia_gpu_present():
+    def is_nvidia_gpu_present() -> bool:
         """
         Checks the presence of an NVIDIA graphics card on the system.
 
