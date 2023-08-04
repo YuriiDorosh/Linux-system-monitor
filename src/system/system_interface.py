d22f9c88e396355c5d94bb6edd7a8944255ab0e7
yurii <yuriydorosh2005@gmail.com>
2023-08-04 18:05:52 +0300

    UPDATE: rewriting imports and instances for new structure of project

--- src/system/system_interface.py
+++ src/system/system_interface.py
@@ -1,4 +1,10 @@
-from . import system_info
+from .checkers import nvidia_checker
+
+from .processor import Processor
+from .battery import Battery
+from .memory import Memory
+from .disk import Disk
+from .video_cards.nvidia import Nvidia
 
 
 class SystemInterface:
@@ -25,7 +31,12 @@ class SystemInterface:
     """
 
     def __init__(self):
-        self.system_info = system_info.SystemInfo()
+        self.processor = Processor()
+        self.battery = Battery()
+        self.memory = Memory()
+        self.disk = Disk()
+        self.nvidia = Nvidia()
+        self.nvidia_checker = nvidia_checker.CheckNvidia()
 
     def get_progress_bars(self):
         """
@@ -34,40 +45,42 @@ class SystemInterface:
         Returns:
             progress_bars (list): A list of progress bars representing CPU load, memory usage, disk usage, and GPU usage.
         """
-        cpu_load = self.system_info.get_cpu_load()
-        memory_usage = self.system_info.get_memory_usage()
-        memory_usage_gb = self.system_info.get_memory_usage_gb()
-        max_memory = self.system_info.get_max_memory()
-        disk_usage = self.system_info.get_disk_usage()
-        gpu_usage = self.system_info.get_gpu_usage()
-        cpu_frequency = self.system_info.get_cpu_frequency()
-        gpu_frequency = self.system_info.get_gpu_frequency()
-        max_cpu_frequency = self.system_info.get_max_cpu_frequency()
-        battery_status = self.system_info.get_battery_status()
+        cpu_load = self.processor.get_cpu_load()
+        memory_usage = self.memory.get_memory_usage()
+        memory_usage_gb = self.memory.get_memory_usage_gb()
+        max_memory = self.memory.get_max_memory()
+        disk_usage = self.disk.get_disk_usage()
+        disk_free_space = self.disk.get_disk_free_space()
+        cpu_frequency = self.processor.get_cpu_frequency()
+        max_cpu_frequency = self.processor.get_max_cpu_frequency()
+        battery_status = self.battery.get_battery_status()
 
-        progress_bars = [f"CPU{i + 1}: {load}%" for i, load in enumerate(cpu_load)] + [
-            f"GPU: {gpu_usage}%",
-            f"CPU Frequency: \n {round(cpu_frequency, 2)} MHz / {max_cpu_frequency} MHz",
-            f"GPU Frequency: {gpu_frequency} MHz",
-            f"RAM: {memory_usage}% ({round(memory_usage_gb, 2)} / {round(max_memory, 2)} GB)",
-            f"Disk: {disk_usage}%",
-        ]
+        gpu_bars = []
+
+        if self.nvidia_checker.is_nvidia_gpu_present():
+            nvidia_usage = self.nvidia.get_gpu_usage()
+            nvidia_frequency = self.nvidia.get_gpu_frequency()
+            gpu_bars.append(f"GPU: {nvidia_usage}%")
+            gpu_bars.append(
+                f"GPU Frequency: {nvidia_frequency} MHz",
+            )
+
+        progress_bars = (
+            [f"CPU{i + 1}: {load}%" for i, load in enumerate(cpu_load)]
+            + gpu_bars
+            + [
+                f"CPU Frequency: \n {round(cpu_frequency, 2)} MHz / {max_cpu_frequency} MHz",
+                f"RAM: {memory_usage}% ({round(memory_usage_gb, 2)} / {round(max_memory, 2)} GB)",
+                f"Disk: {disk_usage}%",
+                f"Disk Free Space: {disk_free_space:.2f} GB",
+            ]
+        )
 
         if battery_status is not None:
-            battery_percent = self.system_info.get_battery_percent()
+            battery_percent = self.battery.get_battery_percent()
             progress_bars.append(f"Battery: {battery_percent}% - {battery_status}")
 
         return progress_bars
-
-    def get_disk_info(self):
-        """
-        Retrieves the disk information and returns it as a string.
-
-        Returns:
-            disk_info (str): Disk free space information.
-        """
-        disk_info = f"Disk Free Space: {self.system_info.get_disk_free_space():.2f} GB"
-        return disk_info
 
 
 class ExtendedSystemInterface(SystemInterface):
@@ -106,7 +119,7 @@ class ExtendedSystemInterface(SystemInterface):
             avg_cpu_load = extended_sys_interface.get_average_cpu_load()
             print(avg_cpu_load)  # Output: "AVG CPU: 45.23%"
         """
-        cpu_load = self.system_info.get_cpu_load()
+        cpu_load = self.processor.get_cpu_load()
         average_load = round(sum(cpu_load) / len(cpu_load), 2)
 
         return f"AVG CPU: {average_load}%"
@@ -122,8 +135,11 @@ class ExtendedSystemInterface(SystemInterface):
             gpu_usage = extended_sys_interface.get_gpu_usage_percentage()
             print(gpu_usage)  # Output: "GPU: 54%"
         """
-        gpu_usage = self.system_info.get_gpu_usage()
-        return f"GPU: {gpu_usage}%"
+        if self.nvidia_checker.is_nvidia_gpu_present():
+            nvidia_usage = self.nvidia.get_gpu_usage()
+            return f"GPU: {nvidia_usage}%"
+        else:
+            return "GPU: no info"
 
     def get_memory_usage_gb(self):
         """
@@ -136,6 +152,6 @@ class ExtendedSystemInterface(SystemInterface):
             memory_usage = extended_sys_interface.get_memory_usage_gb()
             print(memory_usage)  # Output: "RAM: 4.21 GB / 16.0 GB"
         """
-        memory_usage_gb = self.system_info.get_memory_usage_gb()
-        max_memory = self.system_info.get_max_memory()
+        memory_usage_gb = self.memory.get_memory_usage_gb()
+        max_memory = self.memory.get_max_memory()
         return f"RAM: {round(memory_usage_gb, 2)} GB / {round(max_memory, 2)} GB"
