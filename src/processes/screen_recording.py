643b2e836f0a0fe3ab5b79106a7944b600983d51
yurii <yuriydorosh2005@gmail.com>
2023-08-04 23:06:57 +0300

    UPDATE: logging

--- src/processes/screen_recording.py
+++ src/processes/screen_recording.py
@@ -2,41 +2,19 @@ import subprocess
 import datetime
 import os
 import errno
-
+import logging
 import settings
 
-process = None
+logging.basicConfig(level=logging.INFO)
 
 
-def record():
-    """
-    Starts a screen recording using FFmpeg.
+class ScreenRecorder:
+    def __init__(self):
+        self.process = None
 
-    This function initializes a screen recording process using FFmpeg with the specified settings.
-    The output video file will be saved with a timestamped filename in the configured output directory.
-    """
-    global process
-
-    now = datetime.datetime.now()
-    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
-    filename = f"output_{timestamp}.{settings.ScreenRecording.video_extension}"
-
-    # current_path = os.path.dirname(os.path.abspath(__file__))
-    # parent_path = os.path.dirname(current_path)
-
-    # output_path = os.path.join(parent_path, "recordings", filename)
-    # os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    output_path = os.path.join(project_root, "..", "..", "recordings", filename)
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-    print("Starting recording...")
-    print(f"Output Path: {output_path}")
-
-    display_num = os.environ.get("DISPLAY", ":0.0")
-    process = subprocess.Popen(
-        [
+    def _get_command(self, display_num, output_path):
+        """Return the command to start the recording process."""
+        return [
             "ffmpeg",
             "-video_size",
             settings.ScreenRecording.monitor_resolution,
@@ -47,29 +25,41 @@ def record():
             "-i",
             display_num,
             output_path,
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        stdin=subprocess.PIPE,
-        encoding="utf-8",
-    )
-    print("Recording process started.")
+        ]
 
+    def start(self):
+        now = datetime.datetime.now()
+        timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+        filename = f"output_{timestamp}.{settings.ScreenRecording.video_extension}"
 
-def stop_recording():
-    """
-    Stops the screen recording process.
+        project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        output_path = os.path.join(project_root, "..", "recordings", filename)
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
-    This function stops the screen recording process by sending the 'q' command to the FFmpeg process.
-    It flushes the input stream, communicates with the process, and terminates it gracefully.
-    """
-    global process
-    try:
-        process.stdin.write("q")
-        process.stdin.flush()
-        process.communicate()
-        print("Stop Recording")
+        logging.info("Starting recording...")
+        logging.info(f"Output Path: {output_path}")
 
-    except (BrokenPipeError, OSError) as e:
-        if e.errno != errno.EPIPE:
-            raise
+        display_num = os.environ.get("DISPLAY", ":0.0")
+        command = self._get_command(display_num, output_path)
+        self.process = subprocess.Popen(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            encoding="utf-8",
+        )
+        logging.info("Recording process started.")
+
+    def stop(self):
+        """Stop the recording process."""
+        try:
+            if self.process:
+                self.process.stdin.write("q")
+                self.process.stdin.flush()
+                self.process.communicate()
+            logging.info("Stop Recording")
+        except (BrokenPipeError, OSError) as e:
+            if e.errno != errno.EPIPE:
+                raise
+        finally:
+            self.process = None
