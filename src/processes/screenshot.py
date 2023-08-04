1a05c1b89d9ad1ff1148d6ac9f06d4aedb05683d
yurii <yuriydorosh2005@gmail.com>
2023-08-04 23:08:39 +0300

    UPDATE: screenshots logic

---
+++ src/processes/screenshot.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+import os
+
+
+class Screenshot:
+    def __init__(self):
+        # Determine the path for the 'screenshots' directory relative to the current file's location
+        base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        self.save_dir = os.path.join(base_dir, 'screenshots')
+
+        # Create the directory if it doesn't exist
+        os.makedirs(self.save_dir, exist_ok=True)
+
+    def take(self):
+        # Define the name of the screenshot
+        screenshot_name = f"screenshot_{int(time.time())}.png"
+
+        # Define the path where the screenshot will be saved
+        screenshot_path = os.path.join(self.save_dir, screenshot_name)
+
+        # Capture the screenshot
+        screenshot = pyautogui.screenshot()
+
+        # Save the screenshot
+        screenshot.save(screenshot_path)
