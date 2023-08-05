bfd6a6084bf5ae3bf9f986592aa5bf642cccc2f1
yurii <yuriydorosh2005@gmail.com>
2023-08-05 21:44:57 +0300

    ADD: analysis file with logic to check if json file has default configuration

---
+++ src/data_storage/settings/settings_analyzer.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+
+def check_settings_file():
+    settings_path = "settings.json"
+    default_settings = {"cpu_percent_interval": 0.1, "video_format": "mp4", "fps": 30,
+                        "monitor_resolution": "1920x1080", "topmost": "<Control-t>", "start_recording": "<Control-r>",
+                        "minimalize": "<Control-m>", "screenshot": "<Control-s>"}
+
+    if not os.path.isfile(settings_path):
+        with open(settings_path, 'w') as file:
+            json.dump(default_settings, file)
+        return False
+
+    try:
+        with open(settings_path, 'r') as file:
+            settings = json.load(file)
+            if settings == default_settings:
+                return True
+    except json.JSONDecodeError:
+        with open(settings_path, 'w') as file:
+            json.dump(default_settings, file)
+        return False
+
+    return False
