23100d4b49035fd937b95c0929c6952386860299
yurii <yuriydorosh2005@gmail.com>
2023-08-05 23:39:10 +0300

    REFACTOR: documentations

--- src/interfaces/greeting/window.py
+++ src/interfaces/greeting/window.py
@@ -7,9 +7,23 @@ from .. import console, gui, settings_interface
 
 
 class MainWindow(Ctk.CTk):
+    """
+    A class that represents the main window of the application.
 
+    Inherits from Ctk.CTk which is a tkinter wrapper.
+
+    Attributes:
+        img_folder (str): The absolute path of the directory where images are stored.
+        logo (Ctk.CTkImage): An image object representing the logo of the application.
+        logo_label (Ctk.CTkLabel): The label on which the logo is displayed.
+        btn_gui (Ctk.CTkButton): Button to launch the GUI monitor.
+        btn_cons (Ctk.CTkButton): Button to launch the console monitor.
+        btn_settings (Ctk.CTkButton): Button to open the settings.
+
+    """
 
     def __init__(self):
+        """Initializes MainWindow with a set layout and features."""
         super().__init__()
 
         self.img_folder = os.path.abspath("../src/images/project/for-greeting-window")
@@ -51,22 +65,26 @@ class MainWindow(Ctk.CTk):
         self.btn_settings.grid(row=3, column=0, padx=10, pady=10)
 
     def start_gui(self):
+        """Starts the GUI monitor and closes the current window."""
         gui_interface = gui.gui_interface.GuiInterface()
         self.withdraw()
         self.destroy()
         gui_interface.run()
 
     def start_console(self):
+        """Starts the console monitor and closes the current window."""
         console_interface = console.console_interface.ConsoleInterface()
         self.destroy()
         console_interface.run()
 
     def open_settings(self):
+        """Opens the settings window and minimizes the current window."""
         settings_window = settings_interface.settings_window.SettingsWindow()
         self.withdraw()
         self.deiconify()
 
     def run(self):
+        """Runs the main loop of the tkinter interface."""
         self.mainloop()
 
 
