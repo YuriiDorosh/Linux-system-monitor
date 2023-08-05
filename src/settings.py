b8756e846f00a341a108f8680b90fccf5d876e11
yurii <yuriydorosh2005@gmail.com>
2023-08-05 18:12:49 +0300

    FIX: using data from json

--- src/settings.py
+++ src/settings.py
@@ -1,117 +1,56 @@
 import json
 
-
 class SystemInfo:
     """
-    A class representing system information and settings.
+    A class for handling system information settings.
 
     Attributes:
-        cpu_percent_interval (float): The interval at which CPU load is measured, in seconds. By default, it is set to 0.1 seconds.
-            Adjust this value to control the frequency of CPU load updates. Higher values result in slower updates, while lower values result in faster updates.
-
-    Notes:
-        - The `cpu_percent_interval` attribute determines the time interval at which the CPU load is measured.
-          Adjust this value based on your desired frequency of CPU load updates.
+        cpu_percent_interval (float): Interval for measuring CPU load in seconds. Default value is 0.1.
     """
 
-    cpu_percent_interval: float = 0.1  # Default 0.1 (if more - slower, less - faster)
+    cpu_percent_interval: float = 0.1
 
 
 class ScreenRecording:
     """
-    A class representing screen recording functionality.
+    A class for handling screen recording settings.
 
     Attributes:
-        video_extension (str): The default video extension for recorded videos. By default, it is set to "mp4".
-            FFmpeg supports other video formats as well, such as AVI, MKV, MOV, WMV, FLV, WebM, MPEG, 3GP, and TS.
-        frame_rate (int): The frame rate at which the screen recording will be captured. By default, it is set to 30 frames per second (fps).
-            You can modify this value to a smaller or larger frame rate depending on your requirements and system capabilities.
-        monitor_resolution (str): The resolution of the monitor to be recorded. By default, it is set to "1920x1080".
-            You can modify this value to match the desired monitor resolution for your screen recording.
-
-    Usage:
-        To use the `ScreenRecording` class, create an instance of the class and access its attributes and methods.
-
-    Notes:
-        - You can modify the `video_extension` attribute to set a different default video extension for your recordings.
-        - The supported video formats depend on the availability and configuration of FFmpeg on your system.
-        - The `frame_rate` attribute determines the number of frames per second captured in the screen recording.
-          Adjust this value based on your desired output and system capabilities.
-        - The `monitor_resolution` attribute determines the resolution of the monitor to be recorded.
-          Modify this value to match the desired monitor resolution for your screen recording.
+        video_extension (str): Video extension for recording. Default value is "mp4".
+        frame_rate (int): Frame rate for recording. Default value is 30.
+        monitor_resolution (str): Monitor resolution for recording. Default value is "1920x1080".
     """
 
-    video_extension: str = "mp4"  # Default mp4, but FFmpeg supports other formats too:
-    # .avi - Audio Video Interleave video container format.
-    # .mkv - Matroska multimedia container format.
-    # .mov - QuickTime multimedia container format.
-    # .wmv - Windows Media Video format.
-    # .flv - Flash Video format.
-    # .webm - WebM multimedia container format.
-    # .mpeg - MPEG video format.
-    # .3gp - 3GPP multimedia container format.
-    # .ts - MPEG Transport Stream container format.
-
-    frame_rate: int = 30  # Default 30, but you can change this value to a smaller or, if your system allows, to a larger one
-
-    monitor_resolution: str = "1920x1080"  # Default 1920x1080, but you can try:
-    # Standard Definition(SD):
-    #
-    # 640x480
-    # 720x480
-    # 720x576
-    #
-    # High Definition(HD):
-    #
-    # 1280x720(720p)
-    # 1920x1080(1080p)
-    #
-    # Ultra High Definition(UHD) / 4K:
-    #
-    # 3840x2160
-    # 4096x2160
-    #
-    # 5K:
-    #
-    # 5120x2880
-    #
-    # 8K:
-    #
-    # 7680x4320
+    video_extension: str = "mp4"
+    frame_rate: int = 30
+    monitor_resolution: str = "1920x1080"
 
 
 class KeyBindings:
     """
-    A class representing key bindings for various actions in the application.
+    A class for handling key bindings settings.
 
     Attributes:
-        topmost (str): The key binding to toggle the topmost window property. Default is "<Control-t>".
-        start_recording (str): The key binding to start screen recording. Default is "<Control-r>".
-        minimalize (str): The key binding to minimize the window. Default is "<Control-m>".
-        screenshot (str): The key binding to take a screenshot. Default is "<Control-s>".
-
-    Notes:
-        - You can modify the key bindings by changing the value of the corresponding attributes.
-        - Key bindings must be specified in a format recognized by the Tkinter library.
+        topmost (str): Key binding for "topmost" action. Default value is "<Control-t>".
+        start_recording (str): Key binding for "start recording" action. Default value is "<Control-r>".
+        minimalize (str): Key binding for "minimalize" action. Default value is "<Control-m>".
+        screenshot (str): Key binding for "screenshot" action. Default value is "<Control-s>".
     """
+
     topmost = "<Control-t>"
     start_recording = "<Control-r>"
     minimalize = "<Control-m>"
     screenshot = "<Control-s>"
 
 
-# Load data from the JSON file
 def load_settings_from_json(file_path):
     """
-    Load settings from a JSON file.
+    Load settings from a specified JSON file.
 
-    This function reads the contents of the specified JSON file and returns the data as a dictionary.
+    This function will attempt to load settings from the given JSON file and will set default values if the file or specific fields are not found.
 
     Args:
-        file_path (str): The path to the JSON file.
-
-    Returns:
-        dict: A dictionary containing the data loaded from the JSON file.
+        file_path (str): Path to the JSON file containing settings.
 
     Raises:
         FileNotFoundError: If the specified JSON file is not found.
@@ -120,28 +59,19 @@ def load_settings_from_json(file_path):
     try:
         with open(file_path) as json_file:
             data = json.load(json_file)
-            return data
+            SystemInfo.cpu_percent_interval = data.get("cpu_percent_interval", 0.1)
+            ScreenRecording.video_extension = data.get("video_format", "mp4")
+            ScreenRecording.frame_rate = data.get("fps", 30)
+            ScreenRecording.monitor_resolution = data.get("monitor_resolution", "1920x1080")
+            KeyBindings.topmost = data.get("topmost", "<Control-t>")
+            KeyBindings.start_recording = data.get("start_recording", "<Control-r>")
+            KeyBindings.minimalize = data.get("minimalize", "<Control-m>")
+            KeyBindings.screenshot = data.get("screenshot", "<Control-s>")
     except FileNotFoundError:
         print(f"Settings file not found: {file_path}")
-        return {}
     except json.JSONDecodeError:
         print(f"Error decoding JSON in settings file: {file_path}")
-        return {}
 
 
 settings_file_path = "settings.json"
-settings_data = load_settings_from_json(settings_file_path)
-
-# Update attributes in the SystemInfo class
-SystemInfo.cpu_percent_interval = settings_data.get(
-    "cpu_percent_interval", SystemInfo.cpu_percent_interval
-)
-
-# Update attributes in the ScreenRecording class
-ScreenRecording.video_extension = settings_data.get(
-    "video_format", ScreenRecording.video_extension
-)
-ScreenRecording.frame_rate = settings_data.get("fps", ScreenRecording.frame_rate)
-ScreenRecording.monitor_resolution = settings_data.get(
-    "monitor_resolution", ScreenRecording.monitor_resolution
-)
+load_settings_from_json(settings_file_path)
