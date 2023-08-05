f9f4f79cc4444ddcbf903d34ef452bc1b63e054f
yurii <yuriydorosh2005@gmail.com>
2023-08-05 21:51:27 +0300

    FIX: settings file path

--- src/data_storage/settings/settings_analyzer.py
+++ src/data_storage/settings/settings_analyzer.py
@@ -3,7 +3,7 @@ import os
 
 
 def check_settings_file():
-    settings_path = "settings.json"
+    settings_path = "data_storage/settings/settings.json"
     default_settings = {"cpu_percent_interval": 0.1, "video_format": "mp4", "fps": 30,
                         "monitor_resolution": "1920x1080", "topmost": "<Control-t>", "start_recording": "<Control-r>",
                         "minimalize": "<Control-m>", "screenshot": "<Control-s>"}
