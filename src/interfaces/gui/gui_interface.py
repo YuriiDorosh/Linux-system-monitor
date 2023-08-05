ab84752af4ba396b6182b49509bb4277fbc56719
yurii <yuriydorosh2005@gmail.com>
2023-08-05 23:37:17 +0300

    REFACTOR: documentations and typing

--- src/interfaces/gui/gui_interface.py
+++ src/interfaces/gui/gui_interface.py
@@ -10,6 +10,9 @@ logging.basicConfig(filename="screenshot.log", level=logging.INFO)
 
 class GuiInterface:
     def __init__(self):
+        """
+        Initialize the GUI interface and set up initial configurations.
+        """
         self.system_interface = SystemInterface()
         self.extended_system_interface = ExtendedSystemInterface()
 
@@ -29,7 +32,10 @@ class GuiInterface:
         self.create_menu()
         self.create_bindings()
 
-    def create_menu(self):
+    def create_menu(self) -> None:
+        """
+        Create the main menu bar for the application window.
+        """
         self.menu = tk.Menu(self.root)
         self.root.config(menu=self.menu)
 
@@ -50,17 +56,26 @@ class GuiInterface:
         )
         self.menu.add_cascade(label="Tools", menu=self.tools_menu)
 
-    def create_bindings(self):
+    def create_bindings(self) -> None:
+        """
+        Bind keyboard shortcuts for the various commands.
+        """
         self.root.bind_all(KeyBindings.topmost, self.toggle_topmost)
         self.root.bind_all(KeyBindings.start_recording, self.toggle_recording)
         self.root.bind_all(KeyBindings.minimalize, self.toggle_minimalize)
         self.root.bind_all(KeyBindings.screenshot, self.take_screenshot)
 
-    def toggle_topmost(self, event=None):
+    def toggle_topmost(self, event=None) -> None:
+        """
+        Toggle whether the application window is always on top of all other windows.
+        """
         self.topmost = not self.topmost
         self.root.attributes("-topmost", self.topmost)
 
-    def toggle_recording(self, event=None):
+    def toggle_recording(self, event=None) -> None:
+        """
+        Toggle screen recording on or off.
+        """
         self.recording = not self.recording
         recording_label = "Start Recording" if not self.recording else "Stop Recording"
         self.tools_menu.entryconfig(1, label=recording_label)
@@ -69,23 +84,38 @@ class GuiInterface:
         else:
             self.stop_recording()
 
-    def start_recording(self):
+    def start_recording(self) -> None:
+        """
+        Start screen recording.
+        """
         self.screen_recorder = ScreenRecorder()
         self.screen_recorder.start()
 
-    def stop_recording(self):
+    def stop_recording(self) -> None:
+        """
+        Stop screen recording.
+        """
         self.screen_recorder.stop()
 
-    def toggle_minimalize(self, event=None):
+    def toggle_minimalize(self, event=None) -> None:
+        """
+        Toggle whether to minimalize the display of system information.
+        """
         self.minimalize = not self.minimalize
         minimalize_label = "Show Less" if not self.minimalize else "Show More"
         self.tools_menu.entryconfig(2, label=minimalize_label)
 
-    def take_screenshot(self, event=None):
+    def take_screenshot(self, event=None) -> None:
+        """
+        Take a screenshot of the current screen and log the action.
+        """
         self.screenshot.take()
         logging.info("Screenshot taken.")
 
-    def update_gui(self):
+    def update_gui(self) -> None:
+        """
+        Update the system information displayed in the application window every second.
+        """
         if self.minimalize:
             cpu_load = self.extended_system_interface.get_average_cpu_load()
             gpu_usage = self.extended_system_interface.get_gpu_usage_percentage()
@@ -100,6 +130,9 @@ class GuiInterface:
         self.label.config(text=text)
         self.root.after(1000, self.update_gui)
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Start the Tkinter event loop to run the application.
+        """
         self.update_gui()
         self.root.mainloop()
