d680d137295ae55f51237277fc3099238462b460
yurii <yuriydorosh2005@gmail.com>
2023-08-05 22:59:46 +0300

    REFACTOR: documentations and typing

--- src/processes/screenshot.py
+++ src/processes/screenshot.py
@@ -4,7 +4,20 @@ import os
 
 
 class Screenshot:
+    """
+    A class that provides methods to take screenshots.
+
+    Attributes:
+        save_dir (str): The directory where the screenshots are saved.
+
+    Methods:
+        take() -> str: Captures the screenshot and returns the screenshot's file path.
+    """
+
     def __init__(self):
+        """
+        Initializes the Screenshot class by setting up the directory to save the screenshots.
+        """
         # Determine the path for the 'screenshots' directory relative to the current file's location
         base_dir = os.path.dirname(
             os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -12,9 +25,18 @@ class Screenshot:
         self.save_dir = os.path.join(base_dir, "screenshots")
 
         # Create the directory if it doesn't exist
-        os.makedirs(self.save_dir, exist_ok=True)
+        try:
+            os.makedirs(self.save_dir, exist_ok=True)
+        except Exception as e:
+            print(f"Failed to create directory {self.save_dir}. Error: {e}")
 
-    def take(self):
+    def take(self) -> str:
+        """
+        Captures the screenshot and saves it to the directory.
+
+        Returns:
+            str: The file path of the saved screenshot.
+        """
         # Define the name of the screenshot
         screenshot_name = f"screenshot_{int(time.time())}.png"
 
@@ -33,3 +55,4 @@ class Screenshot:
         except Exception as e:
             print(f"Failed to capture and save screenshot. Error: {e}")
             return None
+
