b41d574f80b6bb11ea689bf0afccf9bc757961dc
yurii <yuriydorosh2005@gmail.com>
2023-08-05 23:40:29 +0300

    REFACTOR: documentations

--- src/data_storage/settings/settings_analyzer.py
+++ src/data_storage/settings/settings_analyzer.py
@@ -3,10 +3,37 @@ import os
 
 
 def check_settings_file():
+    """
+    Checks if a settings file exists, validates its contents and creates a new settings file with
+    default values if necessary.
+
+    This function first checks if a settings file exists at the specified location. If the file
+    doesn't exist, a new file with default settings is created.
+
+    Then, it attempts to open and load the settings file. If the contents of the file match the
+    default settings, the function returns True.
+
+    If the file contains invalid JSON data, a JSONDecodeError is thrown. The except clause catches
+    this exception, writes the default settings to the file, and returns False.
+
+    Finally, if the file exists and contains valid JSON that doesn't match the default settings,
+    the function returns False.
+
+    Returns:
+        bool: True if a settings file exists and contains the default settings, False otherwise.
+    """
+
     settings_path = "data_storage/settings/settings.json"
-    default_settings = {"cpu_percent_interval": 0.1, "video_format": "mp4", "fps": 30,
-                        "monitor_resolution": "1920x1080", "topmost": "<Control-t>", "start_recording": "<Control-r>",
-                        "minimalize": "<Control-m>", "screenshot": "<Control-s>"}
+    default_settings = {
+        "cpu_percent_interval": 0.1,
+        "video_format": "mp4",
+        "fps": 30,
+        "monitor_resolution": "1920x1080",
+        "topmost": "<Control-t>",
+        "start_recording": "<Control-r>",
+        "minimalize": "<Control-m>",
+        "screenshot": "<Control-s>",
+    }
 
     if not os.path.isfile(settings_path):
         with open(settings_path, 'w') as file:
