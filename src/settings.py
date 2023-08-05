f4799eae3734b2b94c1204a4ebde972c8281158c
yurii <yuriydorosh2005@gmail.com>
2023-08-05 18:25:27 +0300

    REFACTOR: black

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,6 @@
 import json
 
+
 class SystemInfo:
     """
     A class for handling system information settings.
@@ -62,7 +63,9 @@ def load_settings_from_json(file_path):
             SystemInfo.cpu_percent_interval = data.get("cpu_percent_interval", 0.1)
             ScreenRecording.video_extension = data.get("video_format", "mp4")
             ScreenRecording.frame_rate = data.get("fps", 30)
-            ScreenRecording.monitor_resolution = data.get("monitor_resolution", "1920x1080")
+            ScreenRecording.monitor_resolution = data.get(
+                "monitor_resolution", "1920x1080"
+            )
             KeyBindings.topmost = data.get("topmost", "<Control-t>")
             KeyBindings.start_recording = data.get("start_recording", "<Control-r>")
             KeyBindings.minimalize = data.get("minimalize", "<Control-m>")
