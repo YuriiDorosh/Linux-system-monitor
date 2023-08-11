e7f24e30d47d8ca73bd7e62ba8e8982e67810b2a
Yurii <yuriydorosh2005@gmail.com>
2023-08-11 20:23:26 +0300

    UPDATE: use data from conts.py

--- src/data_storage/settings/settings_analyzer.py
+++ src/data_storage/settings/settings_analyzer.py
@@ -1,5 +1,6 @@
 import json
 import os
+import consts
 from typing import NamedTuple
 
 
@@ -8,33 +9,20 @@ class SettingsStatus(NamedTuple):
     status: str  # Could be "valid", "default", "corrupt"
 
 
-SETTINGS_PATH = "data_storage/settings/settings.json"
-DEFAULT_SETTINGS = {
-    "cpu_percent_interval": 0.1,
-    "video_format": "mp4",
-    "fps": 30,
-    "monitor_resolution": "1920x1080",
-    "topmost": "<Control-t>",
-    "start_recording": "<Control-r>",
-    "minimalize": "<Control-m>",
-    "screenshot": "<Control-s>",
-}
-
-
 def write_default_settings():
-    with open(SETTINGS_PATH, "w") as file:
-        json.dump(DEFAULT_SETTINGS, file)
+    with open(consts.SETTINGS_PATH, "w") as file:
+        json.dump(consts.DEFAULT_SETTINGS, file)
 
 
 def check_settings_file() -> SettingsStatus:
-    if not os.path.isfile(SETTINGS_PATH):
+    if not os.path.isfile(consts.SETTINGS_PATH):
         write_default_settings()
         return SettingsStatus(exists=False, status="default")
 
     try:
-        with open(SETTINGS_PATH, "r") as file:
+        with open(consts.SETTINGS_PATH, "r") as file:
             settings = json.load(file)
-            if settings == DEFAULT_SETTINGS:
+            if settings == consts.DEFAULT_SETTINGS:
                 return SettingsStatus(exists=True, status="default")
             else:
                 return SettingsStatus(exists=True, status="valid")
