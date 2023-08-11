b065d89a568c9eb83ff9e779ceedfd8d2146cb03
Yurii <yuriydorosh2005@gmail.com>
2023-08-11 20:07:57 +0300

    ADD: file with constants

---
+++ src/consts.py
@@ -0,0 +1,12 @@
+SETTINGS_PATH = "data_storage/settings/settings.json"
+
+DEFAULT_SETTINGS = {
+    "cpu_percent_interval": 0.1,
+    "video_format": "mp4",
+    "fps": 30,
+    "monitor_resolution": "1920x1080",
+    "topmost": "<Control-t>",
+    "start_recording": "<Control-r>",
+    "minimalize": "<Control-m>",
+    "screenshot": "<Control-s>",
+}
